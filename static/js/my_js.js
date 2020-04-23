4c7caec35a773ca2f8d0e410cb86d1109429f0a1
Himanshu <himanshudhiman9313@gmail.com>
2020-04-23 17:14:18 +0530

    Templates Changed

--- static/js/my_js.js
+++ static/js/my_js.js
@@ -11,9 +11,9 @@ $(document).on('submit', "#sent_form" , function(e){
         type : "POST",
         data : run_data,
         success : function(json){
-            document.getElementById("pos").innerHTML = json.positive;
-            document.getElementById("neg").innerHTML = json.negative;
-            document.getElementById("neu").innerHTML = json.neutral;
+            document.getElementById("pos").innerHTML = "Positive : " + json.positive;
+            document.getElementById("neg").innerHTML = "Negative : " + json.negative;
+            document.getElementById("neu").innerHTML = "Neutral : " + json.neutral;
         },
         failure : function(data){
             alert("Error Occured");
